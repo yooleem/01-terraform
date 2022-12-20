3ee468a93eec6856e9c335882d12615b8c8b1210
yooleem <leemyoo1213@gmail.com>
2022-12-20 16:22:21 +0900

    20221220

---
+++ count/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+variable "user_names" {
+  description = "Create IAM users with these names"
+  type        = list(string)
+  default     = ["aws04-neo", "aws04-trinity", "aws04-morpheus"]
+}
+
+resource "aws_iam_user" "example" {
+  count = length(var.user_names)
+  name  = var.user_names[count.index]
+}
+
+output "neo_arn" {
+  value = aws_iam_user.example[*].arn
+}
